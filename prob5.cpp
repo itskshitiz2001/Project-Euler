b34557956d7f9d812e0085918b5cf4bb120048e6
Kshitiz <2001kshitizchandra@gmail.com>
2020-01-16 10:55:41 +0530

    #ProjectEuler100

---
+++ prob5.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+long long int smallestmult(long long int a);
+int main()
+{
+    long long int n,sml;
+    cin>>n;
+    sml=smallestmult(n);
+    cout<<sml;
+}
+long long int smallestmult(long long int a)
+{
+    long long int i,j=1,k,small=2000000000;
+    for(i=1;i<=a;i++)
+        j=j*i;
+    for(k=a;k<=j;k=k+a)
+    {
+        long long int flag=0;
+        for(i=1;i<=a;i++)
+        {
+            if(k%i!=0)
+            {
+                flag=1;
+                break;
+            }
+            
+        }
+        if(flag==0&&k<small)
+        small=k;
+    }    
+    return small;
+}
